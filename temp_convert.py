14e9ac8bd010e6eb7d81b5f72cb5acf95a54c228
Mattie Zilaie <your_name@domain.comAccount>
2019-10-02 16:35:12 -0700

    temp change

---
+++ temp_convert.py
@@ -0,0 +1,9 @@
+# Author#Author: Mahtab Zilaie
+#Date: 10/2/2019
+#Description converts Celsius to Fahrenheit temperatures
+
+Celsius = float(input("Please enter a Celsius temperature. "))
+print(Celsius)
+Fahrenheit = ((9/5) * Celsius) + 32
+print("The equivalent Fahrenheit temperature is:")
+print(Fahrenheit) ####figure out how to make this round to one dec
